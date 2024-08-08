913326f5e3e710d281b7588c661ec3216e5119d4
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-08-08 12:12:36 +0000

    dgfdhjkl

---
+++ file.js
@@ -0,0 +1,13 @@
+fetch('./header.html')
+    .then((response) => response.text())
+    .then((data) => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+// for the footer
+
+fetch('./footer.html')
+    .then((response) => response.text())
+    .then((data) => {
+        document.querySelector("footer").innerHTML = data;
+    });
